d3dc2a73c22cfe995157665938ddb7af40811c9e
Gatchan1 <r.barrio.f@gmail.com>
2023-10-14 13:03:52 +0200

    can and must paste stickers

--- js/index.js
+++ js/index.js
@@ -59,10 +59,13 @@ class Sticker {
   print() {
     ctx.drawImage(this.image, this.x, this.y, this.w, this.h);
   }
+  deactivate() {
+    this.active = false;
+  }
 }
 
 document.getElementById("btn-washi1").addEventListener("click", (e) => {
-  stickers.forEach((sticker) => (sticker.active = false));
+  stickers.forEach((sticker) => sticker.deactivate());
   stickers.push(new Sticker());
 });
 
@@ -77,129 +80,129 @@ const update = function () {
 setInterval(update, 60);
 
 document.body.addEventListener("keydown", (e) => {
-  if ("0123456789abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZáéíóúÁÉÍÓÚ.,;:-_~¡!¿?()[]{}<>^'\"`+=/\\%&#$½|@".includes(e.key)) {
-    writing.x += writing.charWidth * writing.backspaceCount;
+  if (!stickers[stickers.length - 1] || !stickers[stickers.length - 1].active) {
+    if ("0123456789abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZáéíóúÁÉÍÓÚ.,;:-_~¡!¿?()[]{}<>^'\"`+=/\\%&#$½|@".includes(e.key)) {
+      writing.x += writing.charWidth * writing.backspaceCount;
 
-    const charIndex = writing.spriteChars.indexOf(e.key);
-    console.log("character index:", e.key, charIndex);
-    let row;
+      const charIndex = writing.spriteChars.indexOf(e.key);
+      console.log("character index:", e.key, charIndex);
+      let row;
 
-    function drawChar() {
-      ctx2.drawImage(
-        sprite, // image
-        (charIndex % 27) * 7, // x position in sprite
-        row * 10, // y position in sprite
-        7, // width in sprite
-        10, // height in sprite
-        writing.x, // x position in canvas
-        writing.y, // y position in canvas
-        writing.charWidth, // width in canvas
-        writing.charHeight // height in canvas
-      );
-      console.log("holaaaa", writing.x);
-    }
-
-    if (writing.x <= 735) {
-      cursor.erase();
-      if (charIndex >= 0 && charIndex < 27) row = 0;
-      if (charIndex >= 27 && charIndex < 54) row = 1;
-      if (charIndex >= 54 && charIndex < 81) row = 2; // (character "~" still not working)
-      if (charIndex >= 81 && charIndex < 108) row = 3;
-      if (charIndex >= 108 && charIndex < 135) row = 4;
-      console.log("x position: ", writing.x);
-      drawChar();
-      writing.x += writing.charWidth;
-      cursor.draw();
-      writing.backspaceCount = 0;
-    } else if (writing.x > 735 && writing.x <= 750) {
-      // Too near to the right limit. Only non alphabetical characters are allowed. (750 is the real limit)
-      if (charIndex >= 0 && charIndex < 64) {
-        reachedEnd.play();
+      function drawChar() {
+        ctx2.drawImage(
+          sprite, // image
+          (charIndex % 27) * 7, // x position in sprite
+          row * 10, // y position in sprite
+          7, // width in sprite
+          10, // height in sprite
+          writing.x, // x position in canvas
+          writing.y, // y position in canvas
+          writing.charWidth, // width in canvas
+          writing.charHeight // height in canvas
+        );
+        console.log("holaaaa", writing.x);
       }
-      if (charIndex >= 64) {
+
+      if (writing.x <= 735) {
         cursor.erase();
-        if (charIndex >= 64 && charIndex < 81) row = 2;
+        if (charIndex >= 0 && charIndex < 27) row = 0;
+        if (charIndex >= 27 && charIndex < 54) row = 1;
+        if (charIndex >= 54 && charIndex < 81) row = 2; // (character "~" still not working)
         if (charIndex >= 81 && charIndex < 108) row = 3;
         if (charIndex >= 108 && charIndex < 135) row = 4;
+        console.log("x position: ", writing.x);
         drawChar();
         writing.x += writing.charWidth;
         cursor.draw();
         writing.backspaceCount = 0;
+      } else if (writing.x > 735 && writing.x <= 750) {
+        // Too near to the right limit. Only non alphabetical characters are allowed. (750 is the real limit)
+        if (charIndex >= 0 && charIndex < 64) {
+          reachedEnd.play();
+        }
+        if (charIndex >= 64) {
+          cursor.erase();
+          if (charIndex >= 64 && charIndex < 81) row = 2;
+          if (charIndex >= 81 && charIndex < 108) row = 3;
+          if (charIndex >= 108 && charIndex < 135) row = 4;
+          drawChar();
+          writing.x += writing.charWidth;
+          cursor.draw();
+          writing.backspaceCount = 0;
+        }
+      } else if (writing.x > 750) {
+        reachedEnd.play();
       }
-    } else if (writing.x > 750) {
-      reachedEnd.play();
     }
-  }
 
-  if (e.key === " ") {
-    e.preventDefault(); // This is for preventing scroll down when pressing the space bar.
-    if (writing.x > 750) {
-      reachedEnd.play();
-    } else {
-      writing.x += writing.charWidth * writing.backspaceCount;
-      writing.backspaceCount = 0;
-      cursor.erase();
-      writing.x += writing.charWidth;
-      cursor.draw();
+    if (e.key === " ") {
+      e.preventDefault(); // This is for preventing scroll down when pressing the space bar.
+      if (writing.x > 750) {
+        reachedEnd.play();
+      } else {
+        writing.x += writing.charWidth * writing.backspaceCount;
+        writing.backspaceCount = 0;
+        cursor.erase();
+        writing.x += writing.charWidth;
+        cursor.draw();
+      }
     }
-  }
-  if (e.key === "Backspace") {
-    if (writing.x > 750) writing.x = 750;
-    writing.x -= writing.charWidth;
-    const crossOut = Math.floor(Math.random() * 7);
-    console.log("borrandoooo", crossOut);
-    // ctx2.fillRect(x,(y-1),writing.charWidth,20);
-    ctx2.drawImage(sprite, crossOut * 7, 4 * 10, 7, 10, writing.x, writing.y, writing.charWidth, writing.charHeight);
-    writing.backspaceCount += 1;
-  }
-  if (e.key === "Enter") {
-    e.preventDefault(); // It would activate the first button on the site (the sticker modal)
-    if (writing.y <= 1020 - writing.initialY) {
-      // lower limit of the page
-      cursor.erase();
-      writing.y += 30;
-      writing.x = writing.initialX;
-      cursor.draw();
-      writing.backspaceCount = 0;
+    if (e.key === "Backspace") {
+      if (writing.x > 750) writing.x = 750;
+      writing.x -= writing.charWidth;
+      const crossOut = Math.floor(Math.random() * 7);
+      console.log("borrandoooo", crossOut);
+      // ctx2.fillRect(x,(y-1),writing.charWidth,20);
+      ctx2.drawImage(sprite, crossOut * 7, 4 * 10, 7, 10, writing.x, writing.y, writing.charWidth, writing.charHeight);
+      writing.backspaceCount += 1;
     }
-    // console.log("this is where we are now: ", y)
-  }
-  if (e.key === "Tab") {
-    e.preventDefault(); // Typing tab targets different buttons across the browser. We want to prevent this.
-    if (writing.x > 735) {
-      reachedEnd.play();
-    } else {
-      writing.x += writing.charWidth * writing.backspaceCount;
-      writing.backspaceCount = 0;
-      cursor.erase();
-      writing.x += writing.initialX;
-      cursor.draw();
+    if (e.key === "Enter") {
+      e.preventDefault(); // It would activate the first button on the site (the sticker modal)
+      if (writing.y <= 1020 - writing.initialY) {
+        // lower limit of the page
+        cursor.erase();
+        writing.y += 30;
+        writing.x = writing.initialX;
+        cursor.draw();
+        writing.backspaceCount = 0;
+      }
+      // console.log("this is where we are now: ", y)
+    }
+    if (e.key === "Tab") {
+      e.preventDefault(); // Typing tab targets different buttons across the browser. We want to prevent this.
+      if (writing.x > 735) {
+        reachedEnd.play();
+      } else {
+        writing.x += writing.charWidth * writing.backspaceCount;
+        writing.backspaceCount = 0;
+        cursor.erase();
+        writing.x += writing.initialX;
+        cursor.draw();
+      }
     }
   }
 
   // ---------------- STICKER KEYDOWNS ----------------
-  if (e.key === "ArrowUp") {
-    e.preventDefault(); // It would make the page scroll up.
-    if (stickers[stickers.length - 1] && stickers[stickers.length - 1].active) {
+  if (stickers[stickers.length - 1] && stickers[stickers.length - 1].active) {
+    if (e.key === "ArrowUp") {
+      e.preventDefault(); // It would make the page scroll up.
       stickers[stickers.length - 1].recalculatePosition(0, -20);
     }
-  }
-  if (e.key === "ArrowRight") {
-    e.preventDefault();
-    if (stickers[stickers.length - 1] && stickers[stickers.length - 1].active) {
+    if (e.key === "ArrowRight") {
+      e.preventDefault();
       stickers[stickers.length - 1].recalculatePosition(20, 0);
     }
-  }
-  if (e.key === "ArrowDown") {
-   e.preventDefault(); // It would make the page scroll down.
-    if (stickers[stickers.length - 1] && stickers[stickers.length - 1].active) {
+    if (e.key === "ArrowDown") {
+      e.preventDefault(); // It would make the page scroll down.
       stickers[stickers.length - 1].recalculatePosition(0, 20);
     }
-  }
-  if (e.key === "ArrowLeft") {
-    e.preventDefault();
-    if (stickers[stickers.length - 1] && stickers[stickers.length - 1].active) {
+    if (e.key === "ArrowLeft") {
+      e.preventDefault();
       stickers[stickers.length - 1].recalculatePosition(-20, 0);
+    }
+    if (e.key === "Enter") {
+      e.preventDefault();
+      stickers[stickers.length - 1].deactivate();
     }
   }
 });
